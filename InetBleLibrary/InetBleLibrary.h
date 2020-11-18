6da0b8b1ede3da7f62d32779d688d98000a358f0
Michael Bielodied <beloded@seniordev.com>
2020-11-18 23:26:35 +0200

    second commit

---
+++ InetBleLibrary/InetBleLibrary.h
@@ -0,0 +1,18 @@
+//
+//  InetBleLibrary.h
+//  InetBleLibrary
+//
+//  Created by Michael Bielodied on 18.11.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InetBleLibrary.
+FOUNDATION_EXPORT double InetBleLibraryVersionNumber;
+
+//! Project version string for InetBleLibrary.
+FOUNDATION_EXPORT const unsigned char InetBleLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InetBleLibrary/PublicHeader.h>
+
+
